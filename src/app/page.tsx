e8d24b8e9a2927f91f5f481cbad2b09975ceac46
Younis1996-code <younisissa205@gmail.com>
2025-10-07 18:59:17 +0300

    heading-paragraph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Heading from "@/components/shared/Typograpgy/Heading";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <h1>hello world</h1>
+      <Heading level={1} size="h72" weight="semibold">hello world</Heading>
     </div>
   );
 }
